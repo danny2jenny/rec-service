92cb9d1cf493d9c8545e6703fa69602b85ad9de6
Danny Gao <12793148@qq.com>
2017-07-13 10:47:56 +0800

    屏蔽Goose通知

--- src/iec61850.c
+++ src/iec61850.c
@@ -185,7 +185,7 @@ void iec61850_parseConfig(char *cfgStr) {
                               0);
     //ReportControlBlock_create("events02", lln0, "events02", false, dataSet, 1, TRG_OPT_DATA_CHANGED, rptOptions, 50, 0);
 
-    GSEControlBlock_create("gse01", lln0, "events01", "events", 1, false, 200, 3000);
+    //GSEControlBlock_create("gse01", lln0, "events01", "events", 1, false, 200, 3000);
 
 
     // 启动服务器
